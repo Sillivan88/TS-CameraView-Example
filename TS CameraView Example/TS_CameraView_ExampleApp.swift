fc56aa4f5db440ab9f611e88d4067faa15b97df2
Thomas Sillmann <contact@thomassillmann.de>
2022-01-21 07:41:47 +0100

    Initial Commit

---
+++ TS CameraView Example/TS_CameraView_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TS_CameraView_ExampleApp.swift
+//  TS CameraView Example
+//
+//  Created by Thomas Sillmann on 21.01.22.
+//
+
+import SwiftUI
+
+@main
+struct TS_CameraView_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
